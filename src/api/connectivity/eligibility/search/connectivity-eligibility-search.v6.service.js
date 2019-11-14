a591aac5e9420ab4331db1b23bafe3d83896fc0f
Stephanie Moallic <stephanie.moallic@corp.ovh.com>
2019-11-14 16:06:19 +0100

    feat(connectivty.eligibility): add polling for buildings by line
    Signed-off-by: Stephanie Moallic <stephanie.moallic@corp.ovh.com>

--- src/api/connectivity/eligibility/search/connectivity-eligibility-search.v6.service.js
+++ src/api/connectivity/eligibility/search/connectivity-eligibility-search.v6.service.js
@@ -1,55 +1,79 @@
-angular.module('ovh-api-services').service('OvhApiConnectivityEligibilitySearchV6', ($resource) => $resource('/connectivity/eligibility/search', {
-}, {
-  searchCities: {
-    url: '/connectivity/eligibility/search/cities',
-    method: 'POST',
-    isArray: false,
-    params: {
-      zipCode: '@zipCode',
+angular.module('ovh-api-services').service('OvhApiConnectivityEligibilitySearchV6', ($resource, Poller) => {
+  const eligibilitySearch = $resource('/connectivity/eligibility/search', {
+  }, {
+    searchCities: {
+      url: '/connectivity/eligibility/search/cities',
+      method: 'POST',
+      isArray: false,
+      params: {
+        zipCode: '@zipCode',
+      },
     },
-  },
-  searchStreets: {
-    url: '/connectivity/eligibility/search/streets',
-    method: 'POST',
-    isArray: false,
-    params: {
-      inseeCode: '@inseeCode',
+    searchStreets: {
+      url: '/connectivity/eligibility/search/streets',
+      method: 'POST',
+      isArray: false,
+      params: {
+        inseeCode: '@inseeCode',
+      },
     },
-  },
-  searchBuildings: {
-    url: '/connectivity/eligibility/search/buildings',
-    method: 'POST',
-    isArray: false,
-    params: {
-      streetCode: '@streetCode',
-      streetNumber: '@streetNumber',
+    searchBuildings: {
+      url: '/connectivity/eligibility/search/buildings',
+      method: 'POST',
+      isArray: false,
+      params: {
+        streetCode: '@streetCode',
+        streetNumber: '@streetNumber',
+      },
     },
-  },
-  searchLines: {
-    url: '/connectivity/eligibility/search/lines',
-    method: 'POST',
-    isArray: false,
-    params: {
-      streetCode: '@streetCode',
-      streetNumber: '@streetNumber',
+    searchLines: {
+      url: '/connectivity/eligibility/search/lines',
+      method: 'POST',
+      isArray: false,
+      params: {
+        streetCode: '@streetCode',
+        streetNumber: '@streetNumber',
+      },
     },
-  },
-  searchBuildingByLines: {
-    url: '/connectivity/eligibility/search/buildingsByLine',
-    method: 'POST',
-    isArray: false,
-    params: {
-      lineNumber: '@lineNumber',
-      status: '@status',
+    searchMeetings: {
+      url: '/connectivity/eligibility/search/meetings',
+      method: 'POST',
+      isArray: false,
+      params: {
+        eligibilityReference: '@eligibilityReference',
+        productCode: '@productCode',
+      },
     },
-  },
-  searchMeetings: {
-    url: '/connectivity/eligibility/search/meetings',
-    method: 'POST',
-    isArray: false,
-    params: {
-      eligibilityReference: '@eligibilityReference',
-      productCode: '@productCode',
-    },
-  },
-}));
+  });
+
+  eligibilitySearch.searchBuildingByLines = function ($scope, opts) {
+    const url = '/connectivity/eligibility/search/buildingsByLine';
+
+    $scope.$on('$destroy', () => {
+      Poller.kill({
+        scope: $scope.$id,
+      });
+    });
+
+    return Poller.poll(
+      url,
+      null,
+      {
+        postData: {
+          lineNumber: opts.lineNumber,
+          status: opts.address,
+        },
+        successRule: {
+          status(elem) {
+            return elem.status === 'error' || elem.status === 'ok';
+          },
+        },
+        scope: $scope.$id,
+        method: 'post',
+        retryMaxAttempts: 3,
+      },
+    );
+  };
+
+  return eligibilitySearch;
+});
